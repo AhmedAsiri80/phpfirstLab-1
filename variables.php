d28f89e83ac9ae1ed08e1a1be1185833900a09f5
ahmed <a707a7med@gmail.com>
2022-03-24 04:25:02 +0300

    variables

---
+++ variables.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$x = 2.5 ;
+$y = 4 ;
+$z = $x + $y ;
+
+echo $x;
+echo "+";
+echo $y;
+echo "=";
+echo $z;
+
+
+//Data Type
+$x = 4; // Integer
+$y = 4.5; // Double
+$myname = "Ahmed"; //String
+$isapsent = true; //Bloen => True / False
+
+//Object
+$no_val = Null; //Null
+
+echo $x;
+
+//
+
+
+?>
